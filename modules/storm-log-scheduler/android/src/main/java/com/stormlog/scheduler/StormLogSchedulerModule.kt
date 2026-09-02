a621aab8f6e400726c6758fc3089c081a4f325b9
IDRTN <idontrememberthename74@gmail.com>
2026-09-02 01:13:36 -0400

    feat: guide users to exact alarm access for precise logging

--- modules/storm-log-scheduler/android/src/main/java/com/stormlog/scheduler/StormLogSchedulerModule.kt
+++ modules/storm-log-scheduler/android/src/main/java/com/stormlog/scheduler/StormLogSchedulerModule.kt
@@ -1,5 +1,10 @@
 package com.stormlog.scheduler
 
+import android.app.AlarmManager
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 
@@ -8,7 +13,18 @@ class StormLogSchedulerModule : Module() {
     Name("StormLogScheduler")
 
     Function("start") { intervalMinutes: Int ->
-      StormLogSchedulerService.start(requireNotNull(appContext.reactContext), intervalMinutes)
+      val context = requireNotNull(appContext.reactContext)
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        val alarmManager = context.getSystemService(AlarmManager::class.java)
+        if (alarmManager != null && !alarmManager.canScheduleExactAlarms()) {
+          appContext.currentActivity?.startActivity(
+            Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM).apply {
+              data = Uri.parse("package:${context.packageName}")
+            }
+          )
+        }
+      }
+      StormLogSchedulerService.start(context, intervalMinutes)
     }
 
     Function("stop") {
