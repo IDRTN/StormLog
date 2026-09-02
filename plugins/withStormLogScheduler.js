ac479794af7dce97093cc6e41db41a37c7718bce
IDRTN <idontrememberthename74@gmail.com>
2026-09-02 01:11:19 -0400

    feat: add native 15-minute monitor scheduler

---
+++ plugins/withStormLogScheduler.js
@@ -0,0 +1,42 @@
+const { withAndroidManifest } = require('expo/config-plugins');
+
+const SCHEDULER_SERVICE = 'com.stormlog.scheduler.StormLogSchedulerService';
+const HEADLESS_SERVICE = 'com.stormlog.scheduler.StormLogHeadlessTaskService';
+
+function ensureService(application, name, attributes = {}) {
+  application.service = application.service || [];
+  const existing = application.service.find(service => service.$?.['android:name'] === name);
+  if (existing) {
+    existing.$ = { ...existing.$, ...attributes };
+    return;
+  }
+  application.service.push({
+    $: {
+      'android:name': name,
+      ...attributes,
+    },
+  });
+}
+
+module.exports = function withStormLogScheduler(config) {
+  return withAndroidManifest(config, configWithManifest => {
+    const application = configWithManifest.modResults.manifest.application?.[0];
+    if (!application) {
+      throw new Error('StormLog scheduler could not find the Android application manifest entry.');
+    }
+
+    ensureService(application, SCHEDULER_SERVICE, {
+      'android:enabled': 'true',
+      'android:exported': 'false',
+      'android:foregroundServiceType': 'location',
+      'android:stopWithTask': 'false',
+    });
+
+    ensureService(application, HEADLESS_SERVICE, {
+      'android:enabled': 'true',
+      'android:exported': 'false',
+    });
+
+    return configWithManifest;
+  });
+};
